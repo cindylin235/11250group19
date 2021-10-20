ac4df682d3b1e4f15f915c0d41727e52090735e7
ZanderBournand <z.bournand@gmail.com>
2021-10-20 02:45:59 -0400

    new build

---
+++ FindMyFish_devBuild/lib/Screens/google_map_screen.dart
@@ -0,0 +1,70 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapScreen extends StatefulWidget {
+  @override
+  _GoogleMapScreenState createState() => _GoogleMapScreenState();
+}
+
+class _GoogleMapScreenState extends State<GoogleMapScreen> {
+
+  Set<Marker> _markers = {};
+  late BitmapDescriptor mapMarker;
+
+  @override
+  void initState() {
+    super.initState();
+    setCustomMarker();
+  }
+
+  void setCustomMarker() async {
+      mapMarker = await BitmapDescriptor.fromAssetImage(ImageConfiguration(), 'Assets/new2.png');
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+      setState(() {
+        _markers.add(
+          Marker(
+              markerId: MarkerId('id-1'),
+              position: LatLng(29.642633,-82.361358),
+              icon: mapMarker,
+              infoWindow: InfoWindow(
+                title: 'Lake Alice',
+                snippet: 'The Swamp',
+              ),
+          ),
+        );
+        _markers.add(
+          Marker(
+            markerId: MarkerId('id-2'),
+            position: LatLng(29.625549,-82.346014),
+            icon: mapMarker,
+            infoWindow: InfoWindow(
+              title: 'Bivens Arm',
+              snippet: 'The Swamp',
+            ),
+          ),
+        );
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Map'),
+      ),
+      body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          markers: _markers,
+          initialCameraPosition: CameraPosition(
+              target: LatLng(29.640619, -82.355532),
+              zoom: 13,
+          )
+      )
+    );
+  }
+}
+
