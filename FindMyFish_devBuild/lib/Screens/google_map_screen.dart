80e4cdce4777deed7478495c097639aa7f749a79
ZanderBournand <z.bournand@gmail.com>
2021-10-24 01:21:04 -0400

    git commit -m 'added onTap functionality for each marker'

--- FindMyFish_devBuild/lib/Screens/google_map_screen.dart
+++ FindMyFish_devBuild/lib/Screens/google_map_screen.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:untitled/Screens/bivens_arm.dart';
+import 'package:untitled/Screens/lake_alice.dart';
 
 class GoogleMapScreen extends StatefulWidget {
   @override
@@ -30,6 +32,10 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
               markerId: MarkerId('Lake_Alice'),
               position: LatLng(29.642633,-82.361358),
               icon: mapMarker,
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LakeAlice())
+              ),
               infoWindow: InfoWindow(
                 title: 'Lake Alice',
                 snippet: 'test spot 1',
@@ -41,6 +47,10 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
             markerId: MarkerId('Bivens Arm'),
             position: LatLng(29.625549,-82.346014),
             icon: mapMarker,
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const BivensArm())
+            ),
             infoWindow: InfoWindow(
               title: 'Bivens Arm',
               snippet: 'test spot 2',
